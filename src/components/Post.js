d31eaf8ec545ef43317c59098b057e5364476d56
Yurii <juracherniak@gmail.com>
2022-03-14 18:56:47 +0100

    make like

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,6 +6,7 @@ import { AuthContext } from "../contexts/authContext";
 import PostsService from "../services/PostsService";
 import { formatDistance } from "date-fns";
 import makeDateAgo from "../helpers/makeDateAgo";
+import { id } from "date-fns/locale";
 
 const Card = styled.div`
     background-color: ${(props) => props.theme.cardBg};
@@ -22,6 +23,11 @@ const PostUserContainer = styled.div`
     & i {
         font-size: 40px;
     }
+    margin-bottom: 10px;
+`;
+const PostUserContainerSpaceBettwen = styled.div`
+    display: flex;
+    justify-content: space-between;
 `;
 const PostUserInfo = styled.div`
     & b {
@@ -40,9 +46,45 @@ const ProfileImg = styled.img`
     width: 40px;
     height: 40px;
 `;
+const ActionContainer = styled.div`
+    border-top: 1px solid ${({ theme }) => theme.borderColor};
+    border-bottom: 1px solid ${({ theme }) => theme.borderColor};
+`;
+const ActionButton = styled.button`
+    color: ${({ theme }) => theme.mainFontColour};
+    padding: 5px;
+    margin: 5px 0;
+    cursor: pointer;
+    width: 50%;
+    border-radius: 5px;
+    background-color: transparent;
+    i {
+        //${(props) => (props.active ? props.theme.buttonColour : "")};
+        color: ${(props) => (props.active ? props.theme.buttonColour : "")};
+    }
+    &:hover {
+        background-color: ${({ theme }) => theme.secondaryBg};
+        i {
+            color: ${({ theme }) => theme.buttonColour};
+        }
+    }
+`;
 const Post = ({ post }) => {
-    const { date, likes, name, recipient, recipientName, text, user, _id } =
-        post;
+    let {
+        date,
+        likes,
+        name,
+        comments,
+        recipient,
+        recipientName,
+        text,
+        user,
+        _id,
+    } = post;
+    const [postLikes, setPostLikes] = useState(likes);
+    const [postComments, setPostComments] = useState(comments);
+    //  console.log(postLikes);
+    const { authState, dispatch } = useContext(AuthContext);
 
     return (
         <Card>
@@ -53,13 +95,77 @@ const Post = ({ post }) => {
                     <i className="fa-regular fa-circle-user"></i>
                 )}
                 <PostUserInfo>
-                    {name}{" "}
-                    {recipient != user._id
-                        ? `posted on ${recipientName} wall`
-                        : null}
+                    <div>
+                        <b>
+                            <Link to={`users/${user._id}`}>{name}</Link>
+                        </b>{" "}
+                        {recipient != user._id ? (
+                            <>
+                                {" "}
+                                posted on{" "}
+                                <b>
+                                    <Link to={`users/${recipient}`}>
+                                        {recipientName}
+                                    </Link>{" "}
+                                </b>
+                                wall
+                            </>
+                        ) : null}
+                    </div>
                     <p>{makeDateAgo(date)} ago</p>
                 </PostUserInfo>
             </PostUserContainer>
+            <PostUserContainer>{text}</PostUserContainer>
+            <PostUserContainerSpaceBettwen>
+                <div>{postLikes.length} likes</div>
+                <div>{postComments.length} comments</div>
+            </PostUserContainerSpaceBettwen>
+            <ActionContainer>
+                <ActionButton
+                    active={postLikes.some(
+                        ({ user }) => user == authState.user._id
+                    )}
+                    onClick={async () => {
+                        //  console.log(authState.user._id);
+                        // console.log(likes);
+                        if (
+                            postLikes.some(
+                                ({ user }) => user == authState.user._id
+                            )
+                        ) {
+                            // postLikes.splice(
+                            //     postLikes.indexOf(authState.user._id),
+                            //     1
+                            // );
+                            setPostLikes(
+                                postLikes.filter(
+                                    ({ user }) => user != authState.user._id
+                                )
+                            );
+                            await PostsService.likePost(_id);
+                            console.log(postLikes);
+                            return;
+                        }
+                        setPostLikes([
+                            ...postLikes,
+                            { user: authState.user._id },
+                        ]);
+                        await PostsService.likePost(_id);
+                        // postLikes.push(authState.user._id);
+                        console.log(postLikes);
+                        return;
+                    }}
+                >
+                    <i className="fas fa-thumbs-up"></i>{" "}
+                    {postLikes.some(({ user }) => user == authState.user._id)
+                        ? "You liked this post"
+                        : "Like"}
+                </ActionButton>
+                <ActionButton>
+                    {" "}
+                    <i className="fas fa-comments"></i> Comment
+                </ActionButton>
+            </ActionContainer>
         </Card>
     );
 };
--- src/services/PostsService.js
+++ src/services/PostsService.js
@@ -4,11 +4,18 @@ export default class PostsService {
     static async getPosts() {
         return $api.get(
             "posts/feed",
-           
+
             {
                 withCredentials: true,
             }
         );
     }
-    
+    static async likePost(postid) {
+        return await $api.post(
+            `posts/${postid}/like`,
+            {
+                withCredentials: true,
+            }
+        );
+    }
 }
